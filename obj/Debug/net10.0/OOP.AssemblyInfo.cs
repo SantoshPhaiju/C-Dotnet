8dda8fb3e2b77c92bc2fc13b57cf2d251409aee4
SantoshPhaiju <santoshphaiju062@gmail.com>
2026-03-25 06:39:48 +0545

    Update assembly informational version and cache inputs

--- obj/Debug/net10.0/OOP.AssemblyInfo.cs
+++ obj/Debug/net10.0/OOP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe3fb9bd846688682c12603f2ef76f45fbd0496c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab2eee6ed30af08c7871a296d1416fbbb97e4c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
